bb7247023d188be4559a9cb941cd400478162d17
Mads Meinert Andersen <mads485d@edu.easj.dk>
2019-02-01 11:28:39 +0100

    Navn & alder i demo-klassen

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
